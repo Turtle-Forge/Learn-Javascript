0e7316126fdf2f1212fb6b10d2e2ffeea7c77559
iambeno1 <benonygabriel40@gmail.com>
2024-04-06 20:07:58 +0700

    belajar output js

---
+++ Javascript Dasar/js/01_Output.js
@@ -0,0 +1,32 @@
+// ============== Inner HTML ==============
+document.getElementById('demo').innerHTML = 5 + 6;
+
+function result() {
+  document.getElementById('result').innerHTML = 10 + 20;
+}
+
+// ============== document.write() ==============
+// document.write(20 + 10);
+
+function write() {
+  document.write(20 + 10);
+}
+
+// ============== window.alert() ==============
+function myAlert() {
+  window.alert(20 + 30);
+}
+
+function myAlert1() {
+  alert('Hello JavaSript');
+}
+
+// ============== console.log() ==============
+function myConsole() {
+  console.log('Test Console');
+}
+
+// ============== window.print() ==============
+function myPrint() {
+  window.print();
+}
